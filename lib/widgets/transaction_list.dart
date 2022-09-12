07fa53dd9feddf4ccf6cf189cb58ed06c39ab395
jcier <syrina02@gmail.com>
2022-09-12 12:24:09 +0200

    Refactoring - added lazy loading to transactions list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -6,51 +6,56 @@ import '../models/transaction.dart';
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
 
-  TransactionList(this.transactions, {Key? key}) : super(key: key);
+  const TransactionList(this.transactions, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Card(
-          child: Row(children: [
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-              decoration: BoxDecoration(
-                  border: Border.all(
-                color: Colors.purple,
-                width: 2,
-              )),
-              padding: const EdgeInsets.all(10),
-              child: Text(
-                '\$${tx.amount.toStringAsPrecision(4)}',
-                style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple),
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            child: Row(children: [
+              Container(
+                margin:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                  color: Colors.purple,
+                  width: 2,
+                )),
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  '\$${transactions[index].amount.toStringAsPrecision(4)}',
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple),
+                ),
               ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  tx.title,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    transactions[index].title,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                Text(
-                  DateFormat.yMMMd().format(tx.date),
-                  style: const TextStyle(
-                    color: Colors.grey,
+                  Text(
+                    DateFormat.yMMMd().format(transactions[index].date),
+                    style: const TextStyle(
+                      color: Colors.grey,
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ]),
-        );
-      }).toList(),
+                ],
+              ),
+            ]),
+          );
+        },
+        itemCount: transactions.length,
+      ),
     );
   }
 }
